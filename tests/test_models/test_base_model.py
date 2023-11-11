63fb2315f1ccab9d72ac11a3c642085cb48c1d13
amch-med23 <skyied.med2023@gmail.com>
2023-11-12 00:47:54 +0100

    uploading the current working code, to be followed by the rest at the partners part.

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" test for the BaseModel class"""
+import unittest
+from models.base_model import BaseModel
+
+class BaseModelTest(unittest.TestCase):
+    """ testing the cases for he base model """
