83e4505f171b3c4044513426af6222b034062fd7
amch-med23 <skyied.med2023@gmail.com>
2023-11-12 05:40:17 +0100

    updating the storage test file

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,29 +1,59 @@
 #!/usr/bin/python3
-"""
-TEST OF CLASS FileStorage
-"""
-from datetime import datetime
+""" handles the storage testing """
 import unittest
-from time import sleep
-import json
+from models import storage
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+import os
 
 
-class test_fileStorage(unittest.TestCase):
-    """Test FileStorage Class"""
-    def test_instances(self):
-        """chequeamos instantation"""
-        obj = FileStorage()
-        self.assertIsInstance(obj, FileStorage)
+class TestFileStorage(unittest.TestCase):
+    '''
+    FileStorage test
+    '''
+    def __init__(self, *args, **kwargs):
+        '''
+        Constructor
+        '''
+        super().__init__(*args, **kwargs)
+        self.test_class = FileStorage
 
-    def test_docs(self):
-        obj = FileStorage()
-        """checking the documentation"""
-        self.assertIsInstance(obj, FileStorage)
-        self.assertIsNotNone(FileStorage.new.__doc__)
-        self.assertIsNotNone(FileStorage.all.__doc__)
-        self.assertIsNotNone(FileStorage.save.__doc__)
-        self.assertIsNotNone(FileStorage.reload.__doc__)
+    def test_pathname(self):
+        '''
+        Attrib test
+        '''
+        test = self.test_class()
+        self.assertIsInstance(test._FileStorage__file_path, str)
 
-    if __name__ == '__main__':
-        unittest.main()
+    def test_file_save(self):
+        '''
+        File creation test
+        '''
+        storage.save()
+        self.assertTrue(os.path.exists('file.json'))
+
+    def test_objects(self):
+        '''
+        Test
+        '''
+        test = self.test_class()
+        self.assertIsInstance(test._FileStorage__objects, dict)
+
+    def tearDown(self):
+        '''
+        Destroy JSON file
+        '''
+        try:
+            os.remove('file.json')
+        except FileNotFoundError:
+            pass
+
+    def test_file_empty(self):
+        '''
+        File empty test
+        '''
+        base = BaseModel()
+        my_dict = base.to_dict()
+        base.save()
+        base2 = BaseModel(**my_dict)
+        self.assertFalse(os.stat('file.json').st_size == 0)
