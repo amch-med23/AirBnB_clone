b07a0b90e119fa3d82e954adf4cf31538a260d7c
amch-med23 <skyied.med2023@gmail.com>
2023-11-12 05:46:26 +0100

    updating tests for strage again

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,74 +1,28 @@
 #!/usr/bin/python3
-""" handles the storage testing """
+"""
+TEST OF CLASS AMENITY
+"""
+from datetime import datetime
 import unittest
-from models import storage
+from time import sleep
+import json
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-import os
 
 
-class TestFileStorage(unittest.TestCase):
-    '''
-    FileStorage test
-    '''
-    def __init__(self, *args, **kwargs):
-        '''
-        Constructor
-        '''
-        super().__init__(*args, **kwargs)
-        self.test_class = FileStorage
-
-    def test_pathname(self):
-        '''
-        Attrib test
-        '''
-        test = self.test_class()
-        self.assertIsInstance(test._FileStorage__file_path, str)
-
-    def test_file_save(self):
-        '''
-        File creation test
-        '''
-        storage.save()
-        self.assertTrue(os.path.exists('file.json'))
-
-    def test_objects(self):
-        '''
-        Test
-        '''
-        test = self.test_class()
-        self.assertIsInstance(test._FileStorage__objects, dict)
-
-    def tearDown(self):
-        '''
-        Destroy JSON file
-        '''
-        try:
-            os.remove('file.json')
-        except FileNotFoundError:
-            pass
-
-    def test_file_empty(self):
-        '''
-        File empty test
-        '''
-        base = BaseModel()
-        my_dict = base.to_dict()
-        base.save()
-        base2 = BaseModel(**my_dict)
-        self.assertFalse(os.stat('file.json').st_size == 0)
+class test_fileStorage(unittest.TestCase):
     """Test FileStorage Class"""
-
     def test_instances(self):
-        """ check instantation"""
+        """chequeamos instantation"""
         obj = FileStorage()
         self.assertIsInstance(obj, FileStorage)
 
     def test_docs(self):
-        """check docmentation """
-        obj = FileStorage()
+        """chequeamos documentacion"""
         self.assertIsInstance(obj, FileStorage)
         self.assertIsNotNone(FileStorage.new.__doc__)        
         self.assertIsNotNone(FileStorage.all.__doc__)
         self.assertIsNotNone(FileStorage.save.__doc__)
         self.assertIsNotNone(FileStorage.reload.__doc__)
+
+    if __name__ == '__main__':
+        unittest.main()
